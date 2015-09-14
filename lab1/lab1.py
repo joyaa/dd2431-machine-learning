6a4c34aa08c15ae523ea0e2859a3f6c2bf407c42
Joy KTH <joya@kth.se>
2015-09-14 18:36:52 +0200

    DONE

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -3,7 +3,7 @@ DD2431 HT15
 Lab 1
 """
 import monkdata as m
-import dtree as tree
+import dtree as t
 import drawtree as draw
 import random
 
@@ -11,7 +11,7 @@ import random
 "--Assignment 1"
 print("--------------------------------------")
 print("Assignment 1: Entropy of training sets")
-monkEntropy = [round(tree.entropy(m.monk1)), round(tree.entropy(m.monk2)), round(tree.entropy(m.monk3))]
+monkEntropy = [round(t.entropy(m.monk1), 5), round(t.entropy(m.monk2), 5), round(t.entropy(m.monk3), 5)]
 "--Answer to Assignment 1"
 print(monkEntropy, "\n") 
 
@@ -25,7 +25,7 @@ att = []; #save values for each attribute
 for monk in monkTrainingSets:	#for each data set
 	for attribute in m.attributes:	#for every attribute
 		#calculate the gain of splitting by the attribute
-		att.append(round(tree.averageGain(monk, attribute),5))
+		att.append(round(t.averageGain(monk, attribute),5))
 
 	informationGain.append(att) #save a "row vector" 
 	att=[]
@@ -33,7 +33,7 @@ for monk in monkTrainingSets:	#for each data set
 "--Answer to Assignment 2"
 print(informationGain[2], "\n")
 
-#print(tree.bestAttribute(m.monk1, m.attributes))
+#print(t.bestAttribute(m.monk1, m.attributes))
 
 """ 
 Attribute a5 has the largest information gain meaning that it reduces the 
@@ -44,15 +44,15 @@ uncertainty the most. Thus, it should be used for splitting at the root node.
 "5 BUILDING DECISION TREES"
 sel=[]
 for i in range(4): #splits data into subset according to attr a5
-	sel.append(tree.select(m.monk1, m.attributes[4], m.attributes[4].values[i]))
+	sel.append(t.select(m.monk1, m.attributes[4], m.attributes[4].values[i]))
 
 #print(sel)
 sub = []
 mC = []
 for subset in sel:
 	for i in [0,1,2,3,5]:
-		sub.append(tree.averageGain(subset, m.attributes[i]));
-	mC.append(tree.mostCommon(subset));
+		sub.append(t.averageGain(subset, m.attributes[i]));
+	mC.append(t.mostCommon(subset));
 	
 	#print(sub)	
 	sub = []
@@ -60,9 +60,9 @@ for subset in sel:
 "Highest information gain on second level of the tree # 2 - A4 , 3 - A6 , 4 - A1 #"
 
 """Assignment 3"""
-tree1 = tree.buildTree(m.monk1, m.attributes)
-tree2 = tree.buildTree(m.monk2, m.attributes)
-tree3 = tree.buildTree(m.monk3, m.attributes)
+tree1 = t.buildTree(m.monk1, m.attributes)
+tree2 = t.buildTree(m.monk2, m.attributes)
+tree3 = t.buildTree(m.monk3, m.attributes)
 
 #draw.drawTree(tree1)
 #draw.drawTree(tree2)
@@ -71,16 +71,16 @@ tree3 = tree.buildTree(m.monk3, m.attributes)
 print("Assignment 3: Decision tree performances")
 
 print("Train errors:")
-print(round(tree.check(tree1, m.monk1),5))
-print(round(tree.check(tree2, m.monk2),5))
-print(round(tree.check(tree3, m.monk3),5))
+print(round(t.check(tree1, m.monk1),5))
+print(round(t.check(tree2, m.monk2),5))
+print(round(t.check(tree3, m.monk3),5))
 
 print("Test errors:")
-print(round(tree.check(tree1, m.monk1test),5))
-print(round(tree.check(tree2, m.monk2test),5))
-print(round(tree.check(tree3, m.monk3test),5))
+print(round(t.check(tree1, m.monk1test),5))
+print(round(t.check(tree2, m.monk2test),5))
+print(round(t.check(tree3, m.monk3test),5), "\n")
 
-#print(tree.mostCommon(tree.select(sel[3],m.attributes[0],3)));
+#print(t.mostCommon(t.select(sel[3],m.attributes[0],3)));
 
 "6 PRUNING"
 def partition(data, fraction):
@@ -89,28 +89,34 @@ def partition(data, fraction):
 	breakPoint = int(len(ldata) * fraction)
 	return ldata[:breakPoint], ldata[breakPoint:]
 
-print("Assignment 4: ")
+print("Assignment 4: Pruning and fractions")
 
 "--Assignment 4"
-fractionError = []
+fractionErrors = []
 fractions = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+
 for monk in [m.monk1, m.monk3]:
-	tempErrors = []
+	tempErrors = [] #temporary errors for the chosen fraction, reset
+
 	for f in fractions:
-		m1train, m1val = partition(monk, f)
+		m1train, m1val = partition(monk, f)	#create new partitioned datasets
+		tree = t.buildTree(m1train, m.attributes)	#create tree with the new datasets
 
-		t1 = tree.buildTree(m1train, m.attributes)
-		currentPerf = tree.check(t1, m1val) 
-		perf1 = 0
-		while perf1 < currentPerf:
-			perf1 = currentPerf
-			for pTree in tree.allPruned(t1):
-				temp = tree.check(pTree, m1val)
-				if currentPerf < temp:
-					currentPerf = temp
-					t1 = pTree
-		#print([perf1,f])
-		tempErrors.append(round(perf1, 5))
-	fractionError.append(tempErrors)
+		tempPerformance = t.check(tree, m1val)	#get current performance of validation set 
+		bestPerformance = 0		#best performance w chosen monk and fraction, set as 0
 
-print(fractionError)
+		while bestPerformance < tempPerformance:	#continue until pruned trees worse than current
+			bestPerformance = tempPerformance	#while loop taken, tempPerformance is best so far
+
+			for pTree in t.allPruned(tree):	
+				prunePerformance = t.check(pTree, m1val)
+
+				if tempPerformance < prunePerformance:
+					tempPerformance = prunePerformance
+					tree = pTree
+
+		tempErrors.append(round(bestPerformance, 5))
+	fractionErrors.append(tempErrors)
+
+print(fractionErrors)
+print("--------------------------------------")
